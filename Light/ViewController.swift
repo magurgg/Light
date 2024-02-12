c1beb14fec149ec70a2b0f6dae866dad0b7482e9
Marco Gutiérrez <marco.gutierrez@accenture.com>
2024-02-12 12:38:40 -0700

    add book implementation

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn: Bool = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        lightOn.toggle()
+        updateUI()
+    }
 }
 
